5470c80977ab3a27b0c93e3f809cfb5baca51c47
valarmo <adilfront3@gmail.com>
2025-05-07 17:09:33 +0100

    just push

--- src/client/views/Welcome.ts
+++ src/client/views/Welcome.ts
@@ -1,14 +1,21 @@
 export function renderWelcome(container: HTMLElement): void {
   const section = document.createElement("section");
-  section.className = "p-8 text-center";
+  section.className = "light-page";
   section.innerHTML = `
-		<button>
-			<i class="fa-solid fa-ticket"></i>
-			Enter The Club
-		</button>
-		<p>“Take your first step into the court”</p>
-		<h1>Welcome to the bhv ping pong club</h1>
-		<h3>Play like it’s 1901. Compete like a champion.</h3>
+		<i class="fa-solid fa-table-tennis-paddle-ball"></i>
+		<i class="fa-solid fa-table-tennis-paddle-ball"></i>
+		<i class="fa-solid fa-table-tennis-paddle-ball"></i>
+		<i class="fa-solid fa-table-tennis-paddle-ball"></i>
+		<i class="fa-solid fa-table-tennis-paddle-ball"></i>
+		<div class="container wlcm-sec">
+			<button class="btn-primary">
+				<i class="fa-solid fa-ticket"></i>
+				Enter The Club
+			</button>
+			<p>“Take your first step into the court”</p>
+			<h1>Welcome to the bhv ping pong club</h1>
+			<h3>Play like it’s 1901. Compete like a champion.</h3>	
+		</div>
 	`;
   container.appendChild(section);
 }
