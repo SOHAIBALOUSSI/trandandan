e794c7aecedad96ba0474ffc8156f8ff13cbfb3f
Mouataz Saadidi <saadidimouataz@gmail.com>
2025-05-22 02:18:27 +0100

    refining nodemailer plugin + still needs to be tested (later ofc)

--- src/server/auth-service/src/plugins/nodemailer-plugin.js
+++ src/server/auth-service/src/plugins/nodemailer-plugin.js
@@ -1,27 +1,47 @@
 import fp from 'fastify-plugin';
-import nodemailer, { createTransport } from 'nodemailer';
+import nodemailer from 'nodemailer';
 
 
 async function nodemailerPlugin(fastify, options) {
-    const testAccount = await nodemailer.createTestAccount();
-    
-    const transporter = nodemailer.createTransport({
-        host: testAccount.smtp.host,
-        port: testAccount.smtp.port,
-        secure: testAccount.smtp.se,
-        auth: {
-            user: testAccount.user,
-            pass: testAccount.pass
-        }
+    let transporterInstance
+    if (process.env.NODE_ENV === 'development')
+    {
+        const testAccount = await nodemailer.createTestAccount();
         
-    })
+        transporterInstance = nodemailer.createTransport({
+            host: testAccount.smtp.host,
+            port: testAccount.smtp.port,
+            secure: testAccount.smtp.secure,
+            auth: {
+                user: testAccount.user,
+                pass: testAccount.pass
+            }
+        })
+    }
+    else if (process.env.NODE_ENV === 'production')
+    {
+        if (!process.env.GMAIL_USER || !process.env.GMAIL_APP_PASS)
+            throw new Error('Missing Gmail credentials in environment');
+        transporterInstance = nodemailer.createTransport({
+            service: 'gmail',
+            auth : {
+                user: process.env.GMAIL_USER,
+                pass: process.env.GMAIL_APP_PASS
+            }
+        })
+    }
 
 
     fastify.decorate('sendMail', async (mailOptions) => {
-        transporter.sendMail(mailOptions)
+        await transporterInstance.sendMail(mailOptions)
         .then((info) => {
-            console.log("Message sent: %s", info.messageId);
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+            console.log("info: ", info);
+            if (process.env.NODE_ENV === 'development')
+            {
+                console.log("Message sent: %s", info.messageId);
+                console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+            }
+            return info;
         })
         .catch(console.error);
     })
