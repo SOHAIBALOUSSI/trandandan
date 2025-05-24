6e8fcc20a2c0ae709a0dc156bc294afd96a9fd6a
M3ayz00 <saadidimouataz@gmail.com>
2025-05-24 21:15:44 +0100

    google sign in (in progress...)

--- src/server/auth-service/src/controllers/googleOAuthController.js
+++ src/server/auth-service/src/controllers/googleOAuthController.js
@@ -1,15 +1,36 @@
-import { google } from 'googleapis'
-
-const oauth2Client = google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
-)
 
 export async function googleSetupHandler(request, reply) {
-    
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&scope=profile email`;
+    reply.redirect(url);
 }
 
 export async function googleLoginHandler(request, reply) {
-    
+    const { code } = request.query;
+    try {
+        const data = await fetch('https://oauth2.googleapis.com/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                code,
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET,
+                redirect_uri: process.env.REDIRECT_URI,
+                grant_type: 'authorization_code'
+            }).toString()
+        });
+
+        const { access_token, id_token } = await data.json();
+        const userinfo  = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            }
+          });
+        const apah = await userinfo.json();
+        console.log(apah);
+    } catch (error) {
+        console.log(error);
+    }
 }
