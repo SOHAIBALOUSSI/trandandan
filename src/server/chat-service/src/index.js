4b0a6ee20bc591ca8c4305ff97b5546a0d290bed
M3ayz00 <saadidimouataz@gmail.com>
2025-06-02 22:44:07 +0100

    chat service is up and running + everything is working together flawlessly + time to refine everything and add security measures + will add documentation for chat later

---
+++ src/server/chat-service/src/index.js
@@ -0,0 +1,132 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { createChatMessagesTable } from './database/initDatabase.js';
+import RabbitMQClient from './libs/rabbitMQ.js';
+import { WebSocketServer, WebSocket } from 'ws';
+import { verifyToken } from './middleware/authMiddleware.js';
+import dotenv from 'dotenv';
+import { addMessage, getAllMessages, getMessageById, markAsDelivered, markAsRead } from './database/chatMessagesDAO.js';
+
+dotenv.config();
+let db;
+
+(async () => {
+  try {
+    db = await open({
+      filename: './messages.db.sqlite',
+      driver: sqlite3.Database
+    });
+    console.log('Database connected.');
+
+    await createChatMessagesTable(db);
+  } catch (error) {
+    console.error('Failed to connect to database or create table:', error);
+    process.exit(1);
+  }
+})();
+
+const users = new Map();
+
+const wss = new WebSocketServer({ port: 3004 });
+
+const rabbit = new RabbitMQClient(process.env.RABBITMQ_QUEUE_NAME);
+
+await rabbit.connect();
+
+wss.on('connection', (ws) => {
+    console.log('WebSocket: Client connected.');
+
+    ws.isAuthenticated = false;
+    ws.userId = null;
+
+    ws.on('message', async (message) => {
+        const payload = JSON.parse(message);
+        if (payload.type === 'AUTHENTICATION') {
+            console.log(`WebSocker: Received this payload:`, payload);
+            verifyToken(ws, payload.token);
+            if (ws.userId) {
+                if (!users.has(ws.userId))
+                users.set(ws.userId, new Set());
+                users.get(ws.userId).add(ws);
+                const messages = await getAllMessages(db, ws.userId);
+                if (messages) {
+                for (const message of messages)
+                    ws.send(JSON.stringify(message));
+                }
+            }
+            else
+                ws.close(3000, 'Unauthorized');
+        }
+        else if (payload.type === 'MESSAGE_SENT') {
+            if (ws.isAuthenticated) {
+                console.log('WebSocket: payload received: ', payload);
+                const recipient = payload.recipient_id;
+                console.log('WebSocket: recipient: ', recipient);
+                if (recipient) {
+                    const messageId = await addMessage(db, payload);
+                    const connections = users.get(recipient);
+                    if (connections) {
+                        for (const conn of connections) {
+                            if (conn.isAuthenticated && conn.readyState === WebSocket.OPEN)
+                                conn.send(JSON.stringify(payload));
+                        }
+                        await markAsDelivered(db, messageId);
+                    }
+                }
+            } else
+                ws.close(3000, 'Unauthorized');
+        }
+        else if (payload.type === 'MESSAGE_READ') {
+            if (ws.isAuthenticated) {
+                console.log('WebSocket: payload received: ', payload);
+                if (payload.id) {
+                    const msg = await getMessageById(db, payload.id)
+                    if (!msg)
+                        ws.close(1008, 'Malformed payload');
+                    await markAsRead(db, msg.id);
+                } else
+                    ws.close(1008, 'Malformed payload');
+            } else
+                ws.close(3000, 'Unauthorized');
+        }
+        else
+            ws.close(1008, 'Malformed payload');
+    })
+
+    ws.on('error', (error) => {
+        console.error('WebSocket: Client error:', error);
+    });
+
+    ws.on('close', () => {
+        console.log('WebSocket: Client disconnected.');
+        if (ws.userId && users.has(ws.userId)) {
+        users.get(ws.userId).delete(ws);
+        if (users.get(ws.userId).size === 0) 
+            users.delete(ws.userId);
+        }
+    })
+
+});
+
+// rabbit.consumeMessages(async (message) =>{
+//   console.log('RabbitMQ: message received: ', message);
+//   const recipient = message.to;
+//   console.log('RabbitMQ: recipient: ', recipient);
+//   if (recipient) {
+//     const messageId = await addMessage(db, message);
+//     const connections = users.get(recipient);
+//     if (connections) {
+//       const message = JSON.stringify(message);
+//       for (const conn of connections) {
+//         if (conn.isAuthenticated && conn.readyState === WebSocket.OPEN)
+//           conn.send(message);
+//       }
+//       await markAsDelivered(db, messageId);
+//     }
+//   }
+// })
+
+wss.on('error', (error) => {
+    console.error('WebSocket: Server error:', error);
+    process.exit(1);
+});
--- src/server/profile-service/src/database/createProfileTable.js
+++ src/server/profile-service/src/database/createProfileTable.js
@@ -7,10 +7,11 @@ export async function createProfileTable(db){
             userId INTEGER NOT NULL UNIQUE,
             username TEXT NOT NULL UNIQUE,
             email TEXT NOT NULL UNIQUE,
-            gender TEXT NOT NULL CHECK(gender IN ('F', 'M'))
+            gender TEXT NOT NULL CHECK(gender IN ('F', 'M')),
             avatar_url TEXT DEFAULT '',
             status TEXT DEFAULT 'offline',
             solde INTEGER DEFAULT 5,
+            level 
             created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
             updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
             )`
