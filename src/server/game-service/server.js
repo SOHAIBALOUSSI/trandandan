99a8f9be3b21af8f8595257dc95e42e63b168e4e
oussama sabbar <sebbaroussama3@gmail.com>
2025-06-22 23:30:38 +0100

    adding rabbitMQ package and creating route for invite system

--- src/server/game-service/server.js
+++ src/server/game-service/server.js
@@ -1,15 +1,15 @@
-"use strict";
 
 import Fastify from "fastify";
 import websocket from "@fastify/websocket";
 import cors from "@fastify/cors";
 import { getUserCookies } from "./routes/cookies.js";
+import  RabbitMGame  from "./routes/RabbitMGame.js"
 
 const fastify = Fastify();
 
 fastify.register(cors, {
   origin: "*", // Allow all origins
-  methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
+  methods: ["GET", "POST", "DELETE"], // Allow specific HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
 });
 
@@ -42,6 +42,36 @@ fastify.register(async function name(fastify) {
     savePlayerData(req, reply);
   });
 });
+
+fastify.register(async function name(fastify) {
+  fastify.post("/invite", async (req, reply) => {
+    const { roomId, senderId, receiverId } = req.body;
+    const rabbitMQ = new RabbitMGame('game'); // Queue name should match what the consumer listens to
+    await rabbitMQ.connect();
+
+    if (!roomId || !senderId || !receiverId) {
+      return reply.code(400).send({ error: "Missing fields" });
+    }
+    try {
+      const message = {
+        type: "INVITE_SENT",
+        data: {
+          roomId,
+          senderId,
+          receiverId,
+          sentAt: new Date().toISOString()
+        }
+      };
+      console.log(message);
+      await rabbitMQ.produceMessage(message, 'game.invite');
+      return reply.code(200).send({ message: "Invite notification queued" });
+    } catch (error) {
+      console.error("Failed to send to RabbitMQ:", error);
+      return reply.code(500).send({ error: "Failed to queue notification" });
+    }
+  });
+});
+
 
 fastify.listen({ port: 5000 , host: '0.0.0.0'}, (err) => {
   if (err) {
