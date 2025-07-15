aa948c0d2d82ffcbb74391db1d6425a0a894dc01
oussama sabbar <sebbaroussama3@gmail.com>
2025-07-15 22:59:11 +0100

    fix: update rank badge logic and use user level for display

--- src/client/components/common/RankBadge.tsx
+++ src/client/components/common/RankBadge.tsx
@@ -1,19 +1,19 @@
 import { styles } from "@/styles/styles";
 
 export function RankBadge(props: { rank: number }) {
-  if (props.rank <= 100) {
+  if (props.rank <= 500) {
     return (
       <span className={styles.badgeStyle}>
         <i className="fa-solid fa-crown text-yellow-400"></i> Club Champion
       </span>
     );
-  } else if (props.rank <= 500) {
+  } else if (props.rank <= 1500) {
     return (
       <span className={styles.badgeStyle}>
         <i className="fa-solid fa-trophy text-orange-400"></i> Elite Contender
       </span>
     );
-  } else if (props.rank <= 1000) {
+  } else if (props.rank <= 2500) {
     return (
       <span className={styles.badgeStyle}>
         <i className="fa-solid fa-medal text-purple-400"></i> Veteran Player
