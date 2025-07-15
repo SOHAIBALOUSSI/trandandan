c48bcfbc517938ae54ce9b9b6836513f3d9da099
oussama sabbar <sebbaroussama3@gmail.com>
2025-07-15 11:59:53 +0100

    fix: update rank thresholds in getUserRank function for accurate user ranking

--- src/client/utils/get-user-rank.ts
+++ src/client/utils/get-user-rank.ts
@@ -1,11 +1,11 @@
 export function getUserRank(rank: number): string {
-  if (rank <= 5) {
+  if (rank <= 100) {
     return "Club Champion";
-  } else if (rank <= 10) {
+  } else if (rank <= 500) {
     return "Elite Contender";
-  } else if (rank <= 20) {
+  } else if (rank <= 1000) {
     return "Veteran Player";
-  } else {
+  } else{
     return "Challenger";
   }
 }
