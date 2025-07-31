1f8bf50282fe4b32b210f4a7b1058e4eb70cc647
oussama sabbar <sebbaroussama3@gmail.com>
2025-07-31 13:25:28 +0100

    feat: implement authentication middleware and secure routes with token verification

--- server/game-service/server.js
+++ server/game-service/server.js
@@ -3,6 +3,8 @@ import Fastify from "fastify";
 import websocket from "@fastify/websocket";
 import cors from "@fastify/cors";
 import redisPlugin from "./routes/redis.js";
+import { verifyToken, verifyWSToken } from "./routes/authMiddleware.js";
+
 const fastify = Fastify();
 
 fastify.register(redisPlugin);
@@ -27,7 +29,20 @@ fastify.register(async function (fastify) {
 // remote games route
 import { remoteGame } from "./routes/remoteGameRoute.js";
 fastify.register(async function (fastify) {
-  fastify.get("/remoteGame", { websocket: true }, (connection, req) => {
+  fastify.get("/remoteGame", { websocket: true }, async (connection, req) => {
+    socket.userId = null;
+    socket.isAuthenticated = false;
+    await verifyWSToken(socket, request, this.redis);
+    if (socket.userId) {
+        if (!onlineUsers.has(socket.userId))
+            onlineUsers.set(socket.userId, new Set());
+        onlineUsers.get(socket.userId).add(socket);
+        displayFriendsStatus(this.db, socket, onlineUsers);
+    }
+    else {
+        socket.close(3000, 'Unauthorized');
+        return ;
+    }    
     remoteGame(connection, req);
   });
 });
@@ -35,56 +50,56 @@ fastify.register(async function (fastify) {
 // route to store the game stats
 import savePlayerData  from "./routes/storePlayerData.js";
 fastify.register(async function name(fastify) {
-  fastify.post("/storePlayerData", (req, reply) => {
+  fastify.post("/storePlayerData", { preHandler: [verifyToken] },(req, reply) => {
     savePlayerData(req, reply);
   });
 });
 
 import invitePlayer from './routes/invite.js';
 fastify.register(async function name(fastify) {
-  fastify.post("/invite", async (req, reply) => {
+  fastify.post("/invite", { preHandler: [verifyToken] }, async (req, reply) => {
     return invitePlayer(req, reply, fastify);
   });
 });
 
 import Accept from "./routes/accept.js";
 fastify.register(async function name(fastify) {
-  fastify.post("/accept", async (req, reply) => {
+  fastify.post("/accept", { preHandler: [verifyToken] }, async (req, reply) => {
     return Accept(req, reply, fastify);
   });
 });
 
 import getRoomId from "./routes/getRoomId.js";
 fastify.register(async function name(req, reply) {
-  fastify.post("/getRoomId", async (req, reply) => {
+  fastify.post("/getRoomId", { preHandler: [verifyToken] }, async (req, reply) => {
     return getRoomId(req, reply, fastify);
   });
 });
 
 import getUserHistory from "./routes/getUserHistory.js";
 fastify.register(async function name(fastify) {
-  fastify.post("/user-history", async (req, reply) => {
+  fastify.post("/user-history", { preHandler: [verifyToken] },async (req, reply) => {
     return getUserHistory(req, reply);
   });
 });
 
 import getData  from "./routes/gethistroy.js";
 fastify.register(async function name(fastify) {
-  fastify.get("/getData", async (req, reply) => {
+  fastify.get("/getData", { preHandler: [verifyToken] },async (req, reply) => {
     return getData(req, reply);
   });
 });
 
 import getLastMatchByUser from "./routes/getLastMatch.js";
 fastify.register(async function name(fastify) {
-  fastify.get("/last-match/:userName", async (req, reply) => {
+  fastify.get("/last-match/:userName", { preHandler: [verifyToken] }, async (req, reply) => {
     return getLastMatchByUser(req, reply);
   });
 });
 
 import getCount from "./routes/getCount.js";
 fastify.register(async function name(fastify) {
-  fastify.get("/user-stats/:userName", async (req, reply) => {
+  fastify.get("/user-stats/:userName", { preHandler: [verifyToken] },async (req, reply) => {
     return getCount(req, reply);
   });
 });
